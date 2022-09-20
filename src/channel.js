7262f876e495c6ed6445111e0408d74c415c87aa
Geoffrey <z5417523@ad.unsw.edu.au>
2022-09-20 14:17:44 +1000

    Channel detail passes test

--- src/channel.js
+++ src/channel.js
@@ -1,7 +1,26 @@
-function channelInviteV1( authUserId, channelId ) {
+function channelDetailsV1( authUserId, channelId ) {
 
     return {
+        name: 'Hayden',
+        ownerMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+        allMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+      }
 
-    }
 }
 
