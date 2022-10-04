c4745213e9988edf00299bde817c8e1113301643
Geoffrey Mok <geoffrey.t.mok@student.unsw.edu.au>
2022-10-05 10:33:35 +1100

    Update channel.js

--- src/channel.js
+++ src/channel.js
@@ -2,50 +2,6 @@ import { getData, setData } from './dataStore.js';
 
 export function channelDetailsV1( authUserId, channelId ) {
 
-  let check_uId = 0 
-  let count2 = 0
-  let count3 = 0
-
-  if (data.users.find(users => users.uid === authUserId)){
-    check_uId = true;
-}
-
-  for (let i of dataStore.academic) {
-    if (academicId === i.academicId) {
-      count += 2;
-    } else {
-      count++;
-    }
-  }
-
-  if (dataStore.course.length != 0) {
-    for (let j of dataStore.course) {
-      if (courseId === j.courseId) {
-        for (let i = 0; i < dataStore.course[j.courseId-1].allMembers.length; i++) {
-          if (academicId === dataStore.course[j.courseId-1].allMembers[i].academicId) {
-            count3 = 1;
-          }
-        }
-      } 
-    }
-  }
-
-  for (let j of dataStore.academic) {
-    if (courseId === j.academicId) {
-      count2 += 2;
-    } else {
-      count2++;
-    }
-  }
-
-  if (count === dataStore.academic.length || count2 === dataStore.academic.length || count3 === 0) {
-    return {error: "error"};
-  } else {
-    return {
-      course: dataStore.course[courseId - 1],
-    };
-  }
-
     return {
         name: 'Hayden',
         ownerMembers: [
