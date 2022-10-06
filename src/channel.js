54181cadb3349c3501562d111169cef09cbcead1
Nicole <z5416632@ad.unsw.edu.au>
2022-10-06 18:29:38 +1100

    added success return test

--- src/channel.js
+++ src/channel.js
@@ -1,3 +1,5 @@
+import { authRegisterV1 } from './auth.js';
+import { channelsCreateV1 } from './channels.js';
 import { getData, setData } from './dataStore.js';
 
 export function channelDetailsV1( authUserId, channelId ) {
@@ -35,11 +37,66 @@ export function channelJoinV1 ( authUserId, channelId ) {
 }
 
 
-export function channelInviteV1( authUserId, channelId ) {
+export function channelInviteV1( authUserId, channelId, uId ) {
+    const data = getData();
+    const userArray = data.users;
+    const channelArray = data.channels;
+    
 
-    return {
+    const channel = data.channels.find(c => c.channelId === channelId);
+    const user1 = data.users.find(a => a.authUserId === authUserId);
+    const user2 = data.users.find(u => u.authUserId === uId);
+    
 
+    // invalid channelId, invalid authUserId, invalid uId
+    if (channel === undefined || user1 === undefined || user2 === undefined) {
+      return {error: 'invalid IDs'};
     }
+
+    /* USE THIS TO GET RID OF CHANNELID = INDEX ASSUMPTION / FIND WHAT INDEX CHANNEL IS IN
+    for (const num1 in channelArray) {
+      if (channelArray[num1].channelId === channelId) {
+        // break
+        const i = num1; // channelId in loops below would be replaced by i;
+      }
+    } */
+
+    // uId is already part of the channel
+    // assumes channelId works the same as a counter (0,1,2...) -> channelId = index of array
+    const allMembersArray = channelArray[channelId].allMembers;
+    for (const num2 in allMembersArray) {
+      if (allMembersArray[num2].authUserId === uId) {
+        return {error: 'uId already part of the channel'};
+      }
+    }
+
+    // authUserId is not part of the channel
+    // still makes the same assumption as above, is the syntax correct?
+    let isMember = false;
+    if (channelArray[channelId].allMembers.find(allMembers => allMembers.authUserId === authUserId)) {
+      isMember = true;
+    }
+    if (isMember === false) {
+      return {error: 'authUserId not part of the channel'};
+    }
+
+    // no errors, pushing the right object to the end of the allMembers array = adding user to channel
+    let j = 0;
+    for (const num3 in userArray) {
+      if (userArray[num3].authUserId === uId) {
+        // break
+        j = num3;
+      }
+    } 
+
+    // testing break works
+/*  console.log("exited for loop, num is");
+    console.log(num); */
+
+    const userData = userArray[j];
+    data.channels[channelId].allMembers.push(userData);
+
+    return {};
 }
 
 export function channelMessagesV1 ( authUserId, channelId, start ){
@@ -110,3 +167,10 @@ console.log(messages_54);
 console.log(messages_54.splice(0,50));
 console.log(messages_54.splice(50,100));
 */
+
+
+const john = authRegisterV1('john.smith@gmail.com', 'password11', 'john', 'smith').authUserId;
+const diane = authRegisterV1('diane.phillip@gmail.com', 'password', 'diane', 'phillip').authUserId;
+const liam = authRegisterV1('liam.hudge@gmail.com', 'password', 'liam', 'hudge').authUserId;
+const channel = channelsCreateV1(john, 'funChannelName', true);
+console.log(channelInviteV1(john, channel.channelId, diane));
