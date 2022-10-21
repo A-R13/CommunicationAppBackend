516b36aacbe712224ef7b021a780784ff77ec172
Dennis Pulickal <z5419608@ad.unsw.edu.au>
2022-10-21 12:30:36 +1100

    Implemented the function

--- src/messages.test.ts
+++ src/messages.test.ts
@@ -2,37 +2,41 @@ import { requestHelper, requestClear } from './other';
 import { requestAuthRegister } from './auth.test';
 
 export function requestDmCreate(token: string, uIds: number[]) {
-    return requestHelper('POST', '/dm/create/v1', { token, uIds });
+  return requestHelper('POST', '/dm/create/v1', { token, uIds });
 }
 
 requestClear();
 
 describe(('DM Create tests'), () => {
-    let user0;
-    let user1;
-    let user2;
-    
+  let user0;
+  //   let user1;
+  //   let user2;
+  //   let user3;
 
-    beforeEach(() => {
-        requestClear();
-        user0 = requestAuthRegister('example0@gmail.com', 'ABCD1234', 'Jeff', 'Doe') as {token: string, authUserId: number};    // uid = 0
-        user1 = requestAuthRegister('example1@gmail.com', 'ABCD1234', 'John', 'Doe') as {token: string, authUserId: number};    // uid = 1
-        user2 = requestAuthRegister('example2@gmail.com', 'ABCD1234', 'Bob', 'Doe') as {token: string, authUserId: number};     // uid = 2
-    });
-    
-    
-    test(('Error returns'), () => {
-        expect(requestDmCreate(user0.token, [0,1,2,3])).toStrictEqual({ error: expect.any(String) });
-        expect(requestDmCreate(user0.token, [0,1,1])).toStrictEqual({ error: expect.any(String) });
-        expect(requestDmCreate('token1', [0,1,2])).toStrictEqual({ error: expect.any(String) });
-    })
+  beforeEach(() => {
+    requestClear();
+    user0 = requestAuthRegister('example0@gmail.com', 'ABCD1234', 'Jeff', 'Doe') as {token: string, authUserId: number}; // uid = 0
+    // user1 =
+    requestAuthRegister('example1@gmail.com', 'ABCD1234', 'John', 'Doe') as {token: string, authUserId: number}; // uid = 1
+    // user2 =
+    requestAuthRegister('example2@gmail.com', 'ABCD1234', 'Bob', 'Doe') as {token: string, authUserId: number}; // uid = 2
+    // user3 =
+    requestAuthRegister('example3@gmail.com', 'ABCD1234', 'Bob', 'Doe') as {token: string, authUserId: number}; // uid = 3
+  });
 
-    test(('Correct returns'), () => {
-        expect(requestDmCreate(user0.token, [0,1,2])).toStrictEqual({ dmId: expect.any(number) });
+  test(('Error returns'), () => {
+    expect(requestDmCreate(user0.token, [1, 2, 3, 4])).toStrictEqual({ error: expect.any(String) });
+    expect(requestDmCreate(user0.token, [1, 1])).toStrictEqual({ error: expect.any(String) });
+    expect(requestDmCreate('token1', [1, 2])).toStrictEqual({ error: expect.any(String) });
+  });
 
-        // requestDmCreate(user0.token, [0,1]);
-        // expect(requestDmDetails(user0.token)) ==> should include only 2 dms
-        // expect(requestDmDetails(user1.token)) ==> should include only 2 dms
-        // expect(requestDmDetails(user2.token)) ==> should include only 1 dm
-    })
-})
+  test(('Correct returns'), () => {
+    expect(requestDmCreate(user0.token, [1, 2, 3])).toStrictEqual({ dmId: expect.any(Number) });
+
+    // requestDmCreate(user0.token, [0,1]);
+    // expect(requestDmDetails(user0.token)) ==> should include only 2 dms
+    // expect(requestDmDetails(user1.token)) ==> should include only 2 dms
+    // expect(requestDmDetails(user2.token)) ==> should include only 1 dm
+    // expect(requestDmDetails(user3.token)) ==> should not include any dms
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 import { authRegisterV2, authLoginV2 } from './auth';
 import { channelDetailsV2, channelJoinV2, channelInviteV2, channelMessagesV2 } from './channel';
 import { channelsCreateV2, channelsListV2, channelsListAllV2 } from './channels';
-import { dmCreateV1 } from './messages'
+import { dmCreateV1 } from './messages';
 import { userProfileV2 } from './users';
 import { clearV1 } from './other';
 
@@ -104,7 +104,7 @@ app.get('/channels/list/v2', (req: Request, res: Response, next) => {
 app.post('/dm/create/v1', (req: Request, res: Response, next) => {
   const { token, uIds } = req.body;
 
-  res.json(dmCreateV1(token, json.parse(uIds)));
+  res.json(dmCreateV1(token, uIds));
 });
 
 // start server
