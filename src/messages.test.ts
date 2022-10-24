0e13a425e391785c7b1d07302a8c2aa76958a82a
Aditya Rana <z5419633@ad.unsw.edu.au>
2022-10-24 15:26:24 +1100

    Added the tests for dmDetailsV1

--- src/messages.test.ts
+++ src/messages.test.ts
@@ -168,3 +168,34 @@ describe('Dm Messages tests', () => {
     // Add more tests when dm message send is done.
   });
 });
+
+describe('Dm details tests', () => {
+  let user0: newUser;
+  let user1: newUser;
+  let user2: newUser;
+  let dm0: newDm;
+
+  beforeEach(() => {
+    requestClear();
+    user0 = requestAuthRegister('example1@gmail.com', 'ABCD1234', 'John', 'Doe'); // uid = 0
+    user1 = requestAuthRegister('example2@gmail.com', 'ABCD1234', 'Bob', 'Doe'); // uid = 1
+    user2 = requestAuthRegister('example0@gmail.com', 'ABCD1234', 'Jeff', 'Doe'); // uid = 2
+    dm0 = requestDmCreate(user0.token, [1]);
+  });
+
+  test('Error (Invalid token)', () => {
+    expect(requestDmDetails('invalid Token', dm0.dmId)).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('Error (Invalid Dmid)', () => {
+    expect(requestDmDetails(user0.token, 67)).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('Error (user is not member of DM)', () => {
+    expect(requestDmDetails(user2.token, dm0.dmId)).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('Successful return', () => {
+    expect(requestDmDetails(user1.token, dm0.dmId)).toStrictEqual({ name: dm0.name, members: dm0.membersArray });
+  });
+});
