e22b900fff9cdd10ed40f8d7b9d01a03a92dc7cd
Nicole <z5416632@ad.unsw.edu.au>
2022-10-28 09:16:51 +1100

    updated to include wrapper function for removeOwner

--- src/channel.test.ts
+++ src/channel.test.ts
@@ -1,16 +1,8 @@
 import { newUser, newChannel } from './other';
 import {
   requestClear, requestAuthRegister, requestChannelsCreate, requestchannelDetails, requestChannelMessages, requestChannelJoin,
-  requestChannelInvite, requestChannelLeave
+  requestChannelInvite, requestChannelLeave, requestRemoveOwner
 } from './wrapperFunctions';
-
-export function requestAddOwner(token: string, channelId: number, uId: number) {
-	return requestHelper('POST', '/channel/addowner/v1', { token, channelId, uId });
-}
-
-export function requestRemoveOwner(token : string, channelId: number, uId: number) {
-  return requestHelper('POST', '/channel/removeowner/v1', { token, channelId, uId });
-}
 
 
 requestClear();
