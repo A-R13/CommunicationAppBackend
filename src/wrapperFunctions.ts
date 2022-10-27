d1c284a34c0d2899f6cbd72775b9ec305df1a581
Nicole <z5416632@ad.unsw.edu.au>
2022-10-28 09:17:09 +1100

    added wrapper function for removeOwner

--- src/wrapperFunctions.ts
+++ src/wrapperFunctions.ts
@@ -107,3 +107,7 @@ export function requestMessageSendDm(token: string, dmId: number, message: strin
 export function requestDmLeave(token: string, dmId: number) {
   return requestHelper('POST', '/dm/leave/v1', { token, dmId });
 }
+
+export function requestRemoveOwner(token: string, channelId: number, uId: number) {
+  return requestHelper('POST', '/channel/removeowner/v1', { token, channelId, uId });
+}
