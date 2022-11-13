c4bf90051ad0b4619bbb57418ee768a4bee67be7
Athavan <z5310246@ad.unsw.edu.au>
2022-11-13 16:21:26 +1100

    fixed a test error

--- src/tests/messageUnpin.test.ts
+++ src/tests/messageUnpin.test.ts
@@ -11,6 +11,84 @@ afterEach(() => {
     requestClear();
 });
 
+describe('Message Unpin tests', () => {
+  let user0: newUser;
+  let user1: newUser;
+  let channel0: newChannel;
+  let dm0: dmType;
+
+  beforeEach(() => {
+    requestClear();
+    user0 = requestAuthRegister('example0@gmail.com', 'ABCD1234', 'Jeff', 'Doe'); // uid = 0
+    user1 = requestAuthRegister('example1@gmail.com', 'ABCD1234', 'John', 'Doe'); // uid = 1
+
+    channel0 = requestChannelsCreate(user0.token, 'Channel1', true);
+    dm0 = requestDmCreate(user0.token, [user1.authUserId]);
+  });
+
+  test('Errors for invalid id and unauthorised user', () => {
+    requestChannelJoin(user1.token, channel0.channelId);
+    const msg1 = requestMessageSend(user0.token, channel0.channelId, 'THE FIRST MESSAGE BY OWNER IN CHANNEL');
+    const msgdm = requestMessageSendDm(user0.token, dm0.dmId, 'FIRST MESSAGE BY THE OWNER IN DM');
+
+    expect(requestMessageUnpin(user0.token, 0)).toStrictEqual(400); // invalid message id
+    expect(requestMessageUnpin('RANDOM TOKEN', msg1.messageId)).toStrictEqual(403); // invalid message id
+    expect(requestMessageUnpin(user1.token, parseInt(msg1.messageId))).toStrictEqual(403); // unauthorised user pinning in channel
+    expect(requestMessageUnpin(user1.token, parseInt(msgdm.messageId))).toStrictEqual(403); // unauthorised user pinning in Dm
+  });
+
+  test('Error if not in channel/dm', () => {
+    const msg1 = requestMessageSend(user0.token, channel0.channelId, 'THE FIRST MESSAGE BY OWNER');
+    const msgdm = requestMessageSendDm(user0.token, dm0.dmId, 'FIRST MESSAGE BY THE OWNER IN DM');
+    const user2: newUser = requestAuthRegister('example5@gmail.com', 'ABCD1234', 'John', 'Doe'); // uid = 2
+
+    expect(requestMessageUnpin(user2.token, msg1.messageId)).toStrictEqual(400);
+    expect(requestMessageUnpin(user2.token, msgdm.messageId)).toStrictEqual(400);
+  });
+
+  test('Error if already pinned', () => {
+    requestChannelJoin(user1.token, channel0.channelId);
+    const msg1 = requestMessageSend(user0.token, channel0.channelId, 'THE FIRST MESSAGE BY OWNER');
+    const msgdm = requestMessageSendDm(user0.token, dm0.dmId, 'FIRST MESSAGE BY THE OWNER IN DM');
+
+    requestMessagePin(user0.token, msg1.messageId);
+    requestMessagePin(user0.token, msgdm.messageId);
+
+    expect(requestMessagePin(user0.token, msg1.messageId)).toStrictEqual(400);
+    expect(requestMessagePin(user0.token, msgdm.messageId)).toStrictEqual(400);
+  });
+
+  test('Correctly Pins a message', () => {
+    const msg1 = requestMessageSend(user0.token, channel0.channelId, 'THE FIRST MESSAGE BY OWNER');
+    const msgdm = requestMessageSendDm(user0.token, dm0.dmId, 'FIRST MESSAGE BY THE OWNER IN DM');
+
+    requestMessagePin(user0.token, msg1.messageId);
+    requestMessagePin(user0.token, msgdm.messageId);
+    expect(requestChannelMessages(user0.token, channel0.channelId, 0).messages).toStrictEqual([
+      {
+        message: 'THE FIRST MESSAGE BY OWNER',
+        messageId: msg1.messageId,
+        uId: user0.authUserId,
+        timeSent: expect.any(Number),
+        reacts: expect.any(Array),
+        isPinned: true,
+      },
+    ]);
+
+    expect(requestDmMessages(user0.token, dm0.dmId, 0).messages).toContainEqual({
+      messageId: expect.any(Number),
+      uId: user0.authUserId,
+      message: 'FIRST MESSAGE BY THE OWNER IN DM',
+      timeSent: expect.any(Number),
+      reacts: expect.any(Array),
+      isPinned: true,
+    });
+  });
+});
+
+
+
+/*
 describe('message unpin test', () => {
     let user0: newUser;
     let user1: newUser;
@@ -22,7 +100,7 @@ describe('message unpin test', () => {
       user0 = requestAuthRegister('example1@gmail.com', 'ABCD1234', 'Bob', 'Doe'); // uid = 0
       user1 = requestAuthRegister('example2@gmail.com', 'ABCD1234', 'John', 'Doe'); // uid = 1
 
-      channel0 = requestChannelsCreate('user0.token', 'channel1', true);
+      channel0 = requestChannelsCreate(user0.token, 'channel1', true);
       dm0 = requestDmCreate(user0.token, [1]);
     });
 
@@ -34,19 +112,19 @@ describe('message unpin test', () => {
       //invalid messageId
       expect(requestMessageUnpin(user0.token, 99)).toStrictEqual(400);
       //invalid token
-      expect(requestMessageUnpin('INVALID TOKEN', msg1.messageId)).toStrictEqual(400);
+      expect(requestMessageUnpin('INVALID TOKEN', msg1.messageId)).toStrictEqual(403);
 
-      expect(requestMessageUnpin('INVALID TOKEN', msg2.messageId)).toStrictEqual(400);
 
-      expect(requestMessageUnpin(user1.token, msg1.messageId)).toStrictEqual(403);
+      expect(requestMessageUnpin(user1.token, parseInt(msg1.messageId))).toStrictEqual(403);
 
-      expect(requestMessageUnpin(user1.token, msg2.messageId)).toStrictEqual(403);
+      expect(requestMessageUnpin(user1.token, parseInt(msg2.messageId))).toStrictEqual(403);
     });
 
-    test('user no in same channel/dm', () => {
-      const user2 = requestAuthRegister('example2@gmail.com', 'ABCD1234', 'Jake', 'Doe');
+
+    test('user not in channel/dm', () => {
       const msg1 = requestMessageSend(user0.token, channel0.channelId, 'Message one in channel');
       const msg2 = requestMessageSendDm(user0.token, dm0.dmId, 'Message one in dm');
+      const user2: newUser = requestAuthRegister('example2@gmail.com', 'ABCD1234', 'Jake', 'Doe');
 
       expect(requestMessageUnpin(user2.token, msg1.messageId)).toStrictEqual(400);
       expect(requestMessageUnpin(user2.token, msg2.messageId)).toStrictEqual(400);
@@ -86,6 +164,7 @@ describe('message unpin test', () => {
           messageId: msg1.messageId,
           uId: user0.authUserId,
           timeSent: expect.any(Number),
+          reacts: expect.any(Array),
           isPinned: true,
         },
       ]);
@@ -95,6 +174,7 @@ describe('message unpin test', () => {
         uId: user0.authUserId,
         message: 'Message one in dm',
         timeSent: expect.any(Number),
+        reacts: expect.any(Array),
         isPinned: true,
       });
 
@@ -107,6 +187,7 @@ describe('message unpin test', () => {
           messageId: msg1.messageId,
           uId: user0.authUserId,
           timeSent: expect.any(Number),
+          reacts: expect.any(Array),
           isPinned: false,
         },
       ]);
@@ -116,8 +197,11 @@ describe('message unpin test', () => {
         uId: user0.authUserId,
         message: 'Message one in dm',
         timeSent: expect.any(Number),
+        reacts: expect.any(Array),
         isPinned: false,
       });
     });
-  });
 
+
+  });
+*/
