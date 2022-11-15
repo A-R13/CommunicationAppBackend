b4c61f3e0c0b561baddcef0f5104f09b3ecca105
Nicole <z5416632@ad.unsw.edu.au>
2022-11-16 10:34:49 +1100

    fixed accidental change error

---
+++ src/tests/authPasswordResetReset.test.ts
@@ -0,0 +1,31 @@
+import { requestClear, requestAuthRegister, requestAuthLogin, requestAuthPasswordResetRequest, requestAuthPasswordResetReset, requestUserProfile } from '../wrapperFunctions';
+import { userType, getData } from '../dataStore';
+
+requestClear(); 
+
+beforeEach(() => {
+  requestClear();
+});
+
+describe('authPasswordResetReset tests', () => {
+  test(('resetCode is not a valid reset code'), () => {
+		requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
+		requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
+		expect(requestAuthPasswordResetReset('invalidcode', 'ABCDE12345')).toStrictEqual(400);
+  });
+
+	test(('newPassword is less than 6 characters long'), () => {
+		const data = getData();
+		requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
+		requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
+		const user = data.users.find(a => a.authUserId === nicole.authUserId);
+		expect(requestAuthPasswordResetReset(user.resetCode, 'short')).toStrictEqual(400);
+	});
+
+	test(('successfully resetted the password'), () => {
+		const data = getData();
+		requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
+		requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
+		expect(requestAuthPasswordResetReset(user.resetCode, 'ABCDE12345')).toStrictEqual({});
+	});
+});
