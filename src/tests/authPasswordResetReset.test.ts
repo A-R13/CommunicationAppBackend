748f9053ce41cdb3fbdad83f36ba3ac0fb314ebb
Nicole <z5416632@ad.unsw.edu.au>
2022-11-16 12:10:55 +1100

    removed success case, test is beyond the scope of our course

--- src/tests/authPasswordResetReset.test.ts
+++ src/tests/authPasswordResetReset.test.ts
@@ -1,4 +1,4 @@
-import { requestClear, requestAuthRegister, requestAuthLogin, requestAuthPasswordResetRequest, requestAuthPasswordResetReset, requestUserProfile } from '../wrapperFunctions';
+import { requestClear, requestAuthRegister, requestAuthLogin, requestAuthPasswordResetRequest, requestAuthPasswordResetReset } from '../wrapperFunctions';
 import { userType, getData } from '../dataStore';
 
 requestClear(); 
@@ -8,27 +8,16 @@ beforeEach(() => {
 });
 
 describe('authPasswordResetReset tests', () => {
-  // test(('resetCode is not a valid reset code'), () => {
-	// 	requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
-	// 	requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
-	// 	expect(requestAuthPasswordResetReset('invalidcode', 'ABCDE12345')).toStrictEqual(400);
-  // });
+  test(('resetCode is not a valid reset code'), () => {
+	requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
+	requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
+	expect(requestAuthPasswordResetReset('invalidcode', 'ABCDE12345')).toStrictEqual(400);
+  });
 
-	// test(('newPassword is less than 6 characters long'), () => {
-	// 	const data = getData();
-	// 	const nicole = requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
-	// 	requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
-	// 	const user = data.users.find(a => a.authUserId === nicole.authUserId);
-	// 	expect(requestAuthPasswordResetReset(user.resetCode, 'short')).toStrictEqual(400);
-	// });
-
-	test(('successfully resetted the password'), () => {
-		const data = getData();
-		const nicole = requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
-		requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
-		// const user = data.users.find(a => a.authUserId === 0);
-		console.log(nicole.token);
-		console.log(requestUserProfile(nicole.token, nicole.authUserId));
-		//expect(requestAuthPasswordResetReset(user.resetCode, 'ABCDE12345')).toStrictEqual({});
-	});
+  test(('newPassword is less than 6 characters long'), () => {
+	const user = requestAuthRegister('nicole.jiang@gmail.com', 'ABCD1234', 'Nicole', 'Jiang');
+	requestAuthPasswordResetRequest('nicole.jiang@gmail.com');
+	expect(requestAuthPasswordResetReset(user.resetCode, 'short')).toStrictEqual(400);
+  })
 });
+
